65868a3b1dc3b9fd65fc4cd807d7fa7d3a79c1b9
Lucas Kanashiro <kanashiro@debian.org>
2018-06-04 13:55:10 -0300

    improve generate_uuid.py: do not load all data in memory

--- generate_uuid.py
+++ generate_uuid.py
@@ -10,10 +10,22 @@ _, filename = argv
 tree = ET.parse(filename)
 trips = tree.getroot()
 
-data = ET.Element('scsimulator_matrix')  
+data = ET.Element('scsimulator_matrix')
 
+output_name = filename.split('.')
+output = open(output_name[0] + '-with-uuid.xml', 'w')
+
+i = 0
 for trip in trips.iter('trip'):
     for i in range(int(trip.attrib['count'])):
+        i += 1
+        if(i > 100):
+            i = 0
+            content = str(ET.tostring(data))
+            output.write(content)
+            data = None
+            data = ET.Element('scsimulator_matrix')
+
         trip_copy = copy.deepcopy(trip)
         uuid_trip = str(uuid.uuid4())
         print(uuid_trip)
@@ -22,7 +34,6 @@ for trip in trips.iter('trip'):
         data.append(trip_copy)
 
 content = str(ET.tostring(data))
-output_name = filename.split('.')
-output = open(output_name[0] + '-with-uuid.xml', 'w')
 output.write(content)
+data = ET.Element('scsimulator_matrix')
 output.close()
