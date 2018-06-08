0f7e27b10faaee7f4363361fb6e7cc227ab10c7a
Lucas Kanashiro <kanashiro@debian.org>
2018-06-08 15:06:36 -0300

    split_trips.py: divide all counts per 2

--- split_trips.py
+++ split_trips.py
@@ -3,6 +3,7 @@
 import xml.etree.ElementTree as ET
 import copy
 from sys import argv
+from math import ceil
 
 _, filename, begin, end = argv
 
@@ -15,13 +16,15 @@ trips = tree.getroot()
 data = ET.Element('scsimulator_matrix')
 
 output_name = filename.split('.')
-output = open(output_name[0] + '-splited.xml', 'w')
+output = open(output_name[0] + '-splitted.xml', 'w')
 
 for trip in trips.iter('trip'):
     start = int(trip.attrib['start'])
+    count = int(trip.attrib['count'])
     if(start >= begin and start <= end):
         trip_copy = copy.deepcopy(trip)
         trip_copy.set('start', str(start - begin))
+        trip_copy.set('count', str(int(ceil(count/2.0))))
         data.append(trip_copy)
 
 content = str(ET.tostring(data))
