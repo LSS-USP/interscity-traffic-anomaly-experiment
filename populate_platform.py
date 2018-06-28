1aa1ea126d1bfb5ebdfdafdfff66766124c7c2ac
DylanGuedes <djmgguedes@gmail.com>
2018-06-28 17:06:51 -0300

    add populate_platform csv compatibility
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- populate_platform.py
+++ populate_platform.py
@@ -4,6 +4,7 @@ import xml.etree.ElementTree as ET
 from sys import argv
 import os
 import requests
+import csv
 
 _, filename = argv
 
@@ -12,12 +13,12 @@ if os.environ.get('INTERSCITY_HOST') is not None:
 else:
     host = 'localhost:8000'
 
-print("[I] Check if parking_monitoring capability exists")
+print("[I] Check if current_location capability exists")
 reponse = requests.get('http://' + host +
                        '/catalog/capabilities/current_location')
 
 if reponse.status_code == 404:
-    print("[I] Creating parking_monitoring capability")
+    print("[I] Creating current_location capability")
     capability_json = { "name": "current_location",
                         "description": "current location",
                         "capability_type": "sensor" }
@@ -26,36 +27,35 @@ if reponse.status_code == 404:
                              json=capability_json)
 
     if response.status_code == 422:
-        print("[E] The capability parking_monitoring was not created")
+        print("[E] The capability current_location was not created")
         exit(-1)
 
-tree = ET.parse(filename)
-trips = tree.getroot()
 
-print("[I] Parsing XML file")
-for trip in trips.iter('trip'):
-    uuid = trip.attrib['uuid']
+with open(filename) as csvfile:
+    reader = csv.reader(csvfile, delimiter=';')
+    for row in reader:
+        uuid = row[7] 
 
-    print("[I] Check if car {0} exists".format(uuid))
-    response = requests.get('http://' + host +
-                            '/catalog/resources/{0}'.format(uuid))
-
-    if response.status_code == 404:
-        print("[I] Create car {0}".format(uuid))
-        car_json = { "data": {
-                                "description": "Car",
-                                "uuid": uuid,
-                                "capabilities": [ "current_location" ],
-                                "status": "active",
-                                "lat": -23,
-                                "lon": -46
-                             }
-                   }
-        
-        response = requests.post('http://' + host + '/catalog/resources',
-                                 json=car_json)
+        print("[I] Check if car {0} exists".format(uuid))
+        response = requests.get('http://' + host +
+                                '/catalog/resources/{0}'.format(uuid))
 
         if response.status_code == 404:
-            print("[E] The car {0} was not created".format(uuid))
+            print("[I] Create car {0}".format(uuid))
+            car_json = { "data": {
+                                    "description": "Car",
+                                    "uuid": uuid,
+                                    "capabilities": [ "current_location" ],
+                                    "status": "active",
+                                    "lat": -23,
+                                    "lon": -46
+                                 }
+                       }
+            
+            response = requests.post('http://' + host + '/catalog/resources',
+                                     json=car_json)
+
+            if response.status_code == 404:
+                print("[E] The car {0} was not created".format(uuid))
 
 print("[I] Finish \o/")
