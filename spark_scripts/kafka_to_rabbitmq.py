f0080f076c09ed19368806fb94ce51496eb78633
DylanGuedes <djmgguedes@gmail.com>
2018-07-03 14:49:35 -0300

    bump kafka to rabbitmq script
    Signed-off-by: DylanGuedes <djmgguedes@gmail.com>

--- spark_scripts/kafka_to_rabbitmq.py
+++ spark_scripts/kafka_to_rabbitmq.py
@@ -51,6 +51,18 @@ edges = load_edges()
 print("edges loaded...")
 
 
+def from_xy_to_latlon(x, y):
+    url = "https://epsg.io/trans?data={0},{1}&s_srs=32719&t_srs=4326".format(x, y)
+    coords = requests.get(url).json()[0]
+    return [float(coords["x"]), float(coords["y"])]
+
+
+def from_latlon_to_xy(lat, lon):
+    url = "https://epsg.io/trans?data={0},{1}&s_srs=4326&t_srs=32719".format(lat, lon)
+    coords = requests.get(url).json()[0] 
+    return [float(coords["x"]), float(coords["y"])]
+
+
 for msg in consumer:
     print("***Anomaly detected***")
     payload = msg.value
@@ -64,15 +76,16 @@ for msg in consumer:
 
     [from_id, to_id] = edges[edge_id]
     coordinates = nodes[from_id]
-    print("corods => ", coordinates)
+    converted_coords = from_xy_to_latlon(coordinates[0], coordinates[1])
+    
     host = "http://localhost:8000/discovery/"
-    endpoint = "resources?capability=traffic_board&lat={0}&lon={1}&radius=5500"\
-            .format(coordinates[0], coordinates[1])
+    endpoint = ("resources?capability=traffic_board&lat={0}&lon={1}&radius=1000"
+            .format(coordinates[0], coordinates[1]))
     resp = requests.get(host + endpoint)
     resources = json.loads(resp.text)["resources"]
     print("resources: ", resources)
+
     for r in resources:
-        print("Resource %s found!" % r)
         description = r.get("description")
         if (description == None):
             raise Exception("""
